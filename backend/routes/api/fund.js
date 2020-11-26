46a173c0f0aea9b4a84050ced2e5256a3d1a8d46
salina kuo <salina.kuo97@gmail.com>
2020-11-25 19:53:07 -0500

    adding fund form inputs

--- backend/routes/api/fund.js
+++ backend/routes/api/fund.js
@@ -13,13 +13,13 @@ router.post('/', asyncHandler(async(req, res) => {
     const fund = await Fund.create({
         title: req.body.title,
         subtitle: req.body.subtitle,
-        description: req.body.subtitle,
-        goal: req.body.subtitle,
-        image: req.body.subtitle,
-        categoryId: req.body.subtitle,
-        ownerId: req.body.subtitle,
+        description: req.body.description,
+        goal: req.body.goal,
+        image: req.body.image,
+        categoryId: req.body.categoryId,
+        ownerId: req.body.ownerId,
     })
-    
+
 }))
 
 module.exports = router;
--- frontend/src/component/FundForm/fundForm.js
+++ frontend/src/component/FundForm/fundForm.js
@@ -1,17 +1,30 @@
 import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import * as fundActions from '../../store/fund';
+import  { receiveCategory } from '../../store/category';
 
 const FundForm = () => {
     const dispatch = useDispatch();
     const [isLoading, setIsLoading] = useState(true);
     const [ showSubtitle, setShowSubtitle ] = useState(false);
     const [ showGoal, setShowGoal ] = useState(false);
+    const [ showCategory, setShowCategory ] = useState(false);
     const [ showDescription, setShowDescription ] = useState(false);
     const [title, setTitle] = useState('');
+    const [subtitle, setSubtitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [goal, setGoal] = useState('');
+    const [categoryId, setCategoryId] = useState(null);
+    const [image, setImage] = useState('');
 
-    // const [ showSubtitle, setShowSubtitle ] = useState(false);
+    useEffect(() => {
+        dispatch(receiveCategory()).then(() => setIsLoading(false))
+    }, [dispatch])
+
+    const categories = useSelector(state => state.category.categories);
+
+    const ownerId = useSelector(state => state.session.user.id);
 
     const showSubtitleInput = (e) => {
         e.preventDefault();
@@ -33,12 +46,15 @@ const FundForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // newFund = {
-        //     title,
-        //     subtitle: newSubtitle,
-        //     goal: newGoal,
-        //     description: newDescription,
-        // }
+        newFund = {
+            title,
+            subtitle,
+            description,
+            goal,
+            image,
+            categoryId,
+            ownerid
+        }
         dispatch(fundActions.makeFund(newFund))
 
     }
@@ -52,19 +68,28 @@ const FundForm = () => {
                 <button onClick={showSubtitleInput}>next</button>
                 {showSubtitle && (
                     <>
-                        <input value={newSubtitle} placeholder='subtitle'></input>
+                        <input value={subtitle} onChange={(e) => setSubtitle(e.target.value)} placeholder='subtitle'></input>
                         <button onClick={showGoalInput}>next</button>
                     </>
                 )}
                 {showGoal && (
                     <>
-                        <input value={newGoal} placeholder='goal'></input>
+                        <input value={goal} onChange={(e) => setGoal(e.target.value)} placeholder='0.00'></input>
                         <button onClick={showDescriptionInput}>next</button>
+                    </>
+                )}
+                {showCategory && (
+                    <>
+                        <select onChange={(e) => setCategoryId(e.target.value)}>
+                            {categories.map((category) => (
+                                <option value={category.id}>{category.name}</option>
+                            ))}
+                        </select>
                     </>
                 )}
                 {showDescription && (
                     <>
-                        <textarea value={newDescription} placeholder='description'></textarea>
+                        <textarea value={description} onChange={(e) => setDescription(e.target.value)} placeholder='description'></textarea>
                         <button type='submit'>Post my fund</button>
                     </>
                 )}
